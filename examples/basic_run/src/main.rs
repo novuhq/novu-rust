d9eef0026baaff26b509625beef667625c14f538
Midka <me@midka.dev>
2022-10-30 14:44:27 +0200

    oops lol

--- examples/basic_run/src/main.rs
+++ examples/basic_run/src/main.rs
@@ -6,13 +6,22 @@ use novu::{
 use std::collections::HashMap;
 #[async_std::main]
 async fn main() {
-    let novu = Novu::new("", None::<String>).unwrap();
+    // Again, this has been reset. So hard to not leak keys lol
+    let novu = Novu::new("2aea87f0ec8c090c64416f75373b1eb3", None::<String>).unwrap();
     let result = novu
         .trigger(TriggerPayload {
             name: "testing".to_string(),
             payload: HashMap::new(),
-            to: TriggerRecipientsType::Single(
-                TriggerRecipient::new("1").first_name("Midka").build(),
+            to: TriggerRecipientsType::Multiple(
+                [
+                    TriggerRecipient::new("1").first_name("Midka").build(),
+                    TriggerRecipient::new("midka@ritta.fi")
+                        .first_name("Midka")
+                        .last_name("Ritta")
+                        .email("midka@ritta.fi")
+                        .build(),
+                ]
+                .to_vec(),
             ),
         })
         .await;
